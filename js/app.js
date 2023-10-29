c6f2b13efbd9640ee1bd16310e53ef5ea7202dc0
Praniket Utturkar <111627350+Praniket-Utturkar@users.noreply.github.com>
2023-10-29 18:37:32 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+let navLinks = document.querySelectorAll('.mobile-nav a');
+
+hamberger.addEventListener('click', function () {
+  mobileNav.classList.add('open');
+});
+
+times.addEventListener('click', function () {
+  mobileNav.classList.remove('open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+  });
+});
+
+
+
+
+
+
